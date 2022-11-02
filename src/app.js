23433b5db4bfe82f3c380e92b365d66011ea8344
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-11-02 02:21:14 +0000

    code listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,73 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  cardGenerator();
 };
+
+let suits = {
+  suit: ["♦", "♥", "♠", "♣"],
+  value: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+};
+
+function cardGenerator() {
+  let selectorValue = Math.floor(Math.random() * suits.value.length); //contiene un numero
+  //ubica el elemento con el selector value
+  let selectorSuit = Math.floor(Math.random() * suits.suit.length);
+  if (selectorSuit == 0 || selectorSuit == 1) {
+    document.querySelector("#valueNumber").classList.add("textRed");
+    document.querySelector(".card-suit").classList.add("textRed");
+    document.querySelector(".card-suit-down").classList.add("textRed");
+  }
+  document.querySelector("#valueNumber").innerHTML = suits.value[selectorValue];
+  document.querySelector(".card-suit").innerHTML = suits.suit[selectorSuit];
+  document.querySelector(".card-suit-down").innerHTML =
+    suits.suit[selectorSuit];
+}
+
+/*
+  const randomTitle = () => {
+  let titles = ["♦", "♥", "♠", "♣"];
+  return titles[Math.floor(Math.random() * titles.length)];
+};
+
+const changeTitle = h1 => {
+  let newTitle = randomTitle();
+  while (newTitle == h1.innerHTML) {
+    newTitle = randomTitle();
+  }
+  return newTitle;
+};
+
+window.onload = function() {
+  //write your code here
+  let titles = document.querySelectorAll(".title");
+  let h1;
+  let randomTitleText = randomTitle();
+  for (let title of titles) {
+    h1 = document.createElement("h1");
+    h1.innerHTML = randomTitleText;
+    title.innerHTML = "";
+    title.appendChild(h1);
+  }
+
+  let footer = document.getElementById("footer");
+  footer.innerHTML = "";
+  let button = document.createElement("button");
+  button.innerHTML = "Cambia de título";
+  footer.appendChild(button);
+
+  // let interval = setInterval(() => {
+  //   let newTitle = changeTitle(h1);
+  //   h1.innerHTML = newTitle;
+  // }, 1000);
+
+  // button.addEventListener("click", () => {
+  //   let newTitle = changeTitle(h1);
+  //   h1.innerHTML = newTitle;
+  //   clearInterval(interval);
+  //   interval = setInterval(() => {
+  //     let newTitle = changeTitle(h1);
+  //     h1.innerHTML = newTitle;
+  //   }, 1000);
+  // });
+}; */
